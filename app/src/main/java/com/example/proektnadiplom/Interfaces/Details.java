18531d7b9f026b158a2b475dae389e8cb18fd57f
Discorre <91318121+Discorre@users.noreply.github.com>
2022-05-24 16:32:30 +0700

    Commit2

---
+++ app/src/main/java/com/example/proektnadiplom/Interfaces/Details.java
@@ -0,0 +1,71 @@
+package com.example.proektnadiplom.Interfaces;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+
+public class Details extends AppCompatActivity {
+        AppDatabaseUsers library = Room.databaseBuilder(getApplicationContext(),
+            AppDatabaseUsers.class, "library")
+            .allowMainThreadQueries()
+            .build();
+
+
+    public static Details instance;
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        instance = this;
+        library = Room.databaseBuilder(this, AppDatabaseUsers.class, "library").build();
+    }
+
+    public static Details getInstance() {
+        return instance;
+    }
+
+    public AppDatabaseUsers getDatabase() {
+        return library;
+    }
+}
+
+
+    /*private String newname;
+    private String newfamile;
+    private String newsurname;
+    private int newnumpass;
+    private String newbirthday;
+
+    public Details(String newname, String newfamile, String newsurname, int newnumpass, String newbirthday) {
+        this.newname = newname;
+        this.newfamile = newfamile;
+        this.newsurname = newsurname;
+        this.newnumpass = newnumpass;
+        this.newbirthday = newbirthday;
+    }
+
+    public String getNewname() {
+        return newname;
+    }
+
+    public String getNewfamile() {
+        return newfamile;
+    }
+
+    public String getNewsurname() {
+        return newsurname;
+    }
+
+    public int getNewnumpass() {
+        return newnumpass;
+    }
+
+    public String getNewbirthday() {
+        return newbirthday;
+    }
+}*/
+
